327a598a26874ffff94b7380ae0543a56772aa1d
davletmegahorosh <azimovdavlet460@gmail.com>
2024-11-30 20:37:32 +0600

    user and buing musics

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,21 @@
 public class Main {
 
     public static void main(String[] args) {
+//
+        Music symphony = new Music("symphony", "Clean Bandit", "pop", 246, 40);
+        Music subeme = new Music("subele a la radio", "Enrique", "pop", 208, 20);
+        Music night_call = new Music("Night call","Kavinsky", "pop", 256, 55);
 
-        Music symphony = new Music("symphony", "Clean Bandit", "pop", 246);
-        Music subeme = new Music("subele a la radio", "Enrique", "pop", 208);
-        Music night_call = new Music("Night call","Kavinsky", "pop", 256);
 
-        System.out.println(symphony.get_info());
-        System.out.println(subeme.get_info());
-        System.out.println(night_call.get_info());
+        User Ryan = new User("Ryan Gosling", "POP", 500);
+        User Temi = new User("Temirlan Emilbekov", "Metal", 120);
+
+//        Ryan.getInfo();
+//        Temi.getInfo();
+
+        Ryan.getBalance();
+        Ryan.buyMusic(night_call);
+        Ryan.getBalance();
     }
 
 }
--- src/Music.java
+++ src/Music.java
@@ -3,17 +3,23 @@ public class Music {
     String author;
     String genre;
     Integer duration_sec;
+    double price;
 
-    public Music(String name, String author, String genre, Integer duration_sec){
+    public Music(String name, String author, String genre, Integer duration_sec, double price){
         this.name = name;
         this.author = author;
         this.genre = genre;
         this.duration_sec = duration_sec;
+        this.price = price;
     }
 
-    public String get_info(){
-        return this.name + " by " + this.author + ". genre: " + this.genre;
+    public void get_info(){
+        System.out.println(this.name + " by " + this.author + ". genre: " + this.genre + ". Costs " +this.price);
+
     }
 
+    public String toString(){
+        return this.name + " by " + this.author ;
+    }
 
 }
